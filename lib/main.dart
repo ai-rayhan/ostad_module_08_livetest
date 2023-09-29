1efa7c9500be27f483febaf867c532e5895fc788
RayhanAhmed <rayhanahmed1127@gmail.com>
2023-09-29 23:36:30 +0600

    livetest

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Image Feed'),
+          title: const Text('News Feed'),
         ),
         body: const ImageFeed(),
       ),
